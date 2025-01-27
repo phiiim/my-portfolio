5601df8afbe06631cebf0606f89371d4d7ac0d6c
“phiiim” <101024240+phiiim@users.noreply.github.com>
2025-01-27 23:30:33 +0000

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['lucide-react'],
-    },
-  },
+  base: '/my-portfolio/'
 })
